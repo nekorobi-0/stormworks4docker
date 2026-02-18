47c04e5aac58638c160aec8f570fb61538fbf078
null <kamiya_4783@yahoo.co.jp>
2026-02-18 22:07:24 +0900

    a

--- ContainerdStormworks.old/devscript.sh
+++ ContainerdStormworks.old/devscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 docker build -t stormworks-dedicatedserver-docker:latest .
-docker run --rm -it stormworks-dedicatedserver-docker:latest
+docker run --rm -it --privileged --sysctl kernel.unprivileged_userns_clone=1 --security-opt seccomp=unconfined --security-opt apparmor=unconfined stormworks-dedicatedserver-docker:latest
